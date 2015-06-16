574fc7dce12808de466a9485f771bcad77a74ff6
Niko Rosvall <niko@seravo.fi>
2015-06-16 12:27:20 +0300

    Fix and change dashboard CSS layout.
    Dashboard widget should be more clean now and scale well on different browsers.

--- widget.php
+++ widget.php
@@ -1,52 +1,21 @@
-<?php
-/**
- * This file could be used to catch submitted form data. When using a non-configuration
- * view to save form data, remember to use some kind of identifying field in your form.
- */
 
-/*<canvas id="lineChart" style="width:100%; height:100%"></canvas>
-<canvas id="barChart" style="width:100%; height:100%"></canvas>
-
-
-
-<div id="wpdrs_content">
-    <div class="chartData">
-      <div class="chart"><canvas id="lineChart" style="width:100%; height:100%"></canvas></div>
-      <div class="numericData">12312312132</div>
-    </div>
-    <div class="chartData">
-       <div class="chart"><canvas id="barChart" style="width:100%; height:100%"></canvas></div>
-      <div class="numericData">12312312313</div>
-</div>
-</div>*/
-
-/*<div id="wpdrs_content">
-<table>
-
-<tr>
-  <td><canvas id="lineChart" style="width:100%; height:100%"></canvas></td>
-  <td><div class="numericData">asdasd</div></td>
-</tr>
-<tr>
-  <td><canvas id="barChart" style="width:100%; height:100%"></canvas></td>
-  <td><div class="numericData">asdasd</div></td>
-</tr>
-
-</table>
-</div>
-*/?>
 <div class="wpdrsContainer">
-  <div class="row">
-    <div class="column"><canvas id="lineChart" style="width:100%; height:150%"></canvas></div>
-    <div class="column"><div id="lineChartAvg" class="numericData"></div></div>
-  </div>  
-  <div class="row">
-    <div class="column"><canvas id="barChart" style="width:100%; height:150%"></canvas></div>
-    <div class="column"><div id="barChartAvg"class="numericData">15ms</div></div>
-  </div>  
+
+    
+    <div><canvas id="lineChart"></canvas></div>
+    <div id="lineChartAvg">Average: 11ms</div>
+   
+    <div><canvas id="barChart"></canvas></div>
+    <div id="barChartAvg">Average: 15ms</div>
+    
+    <div id="submit-button">
+	<input id="btnSubmit" type="submit"/>
+    </div>
+    
 </div>
-<div class="demo">
-<br> <br> <br>   
-<input id = "btnSubmit" type="submit"/>
-<br> <br> <br>  
-</div>
+
+
+
+
+
+
