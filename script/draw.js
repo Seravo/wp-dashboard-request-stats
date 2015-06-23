6567b8ad8537984dd841aae4105f99e69488587d
Tarppa <tari@seravo.fi>
2015-06-23 16:10:07 +0300

    tidying up of the code

--- script/draw.js
+++ script/draw.js
@@ -61,10 +61,9 @@ $( document ).ready(function(){
     lineAvg = Math.round(countAvg(lineValue));
     $("#lineChartAvg").text('Number of requests per day ( average ' + lineAvg + ' )');
     $("#barChartAvg").text('Average response time per day ( week average ' + barAvg.toFixed(3) + 's )');
-  
-  
   });
   
+  //button functionality
   $("#btnSubmit1").click(function(){
     submitDays(3);
   });
@@ -75,7 +74,7 @@ $( document ).ready(function(){
     submitDays(30);
   });  
   
-  
+
   function submitDays( amount ){
     //
     ajaxData = {'action': 'get_chart_data','amount' : amount}; 
@@ -132,142 +131,11 @@ $( document ).ready(function(){
       barAvg = countAvg(barValue);
       lineAvg = Math.round(countAvg(lineValue));
       $("#lineChartAvg").text('Number of requests per day ( average ' + lineAvg + ' )');
-      $("#barChartAvg").text('Average response time per day ( week average ' + barAvg.toFixed(3) + 's )');
+      $("#barChartAvg").text('Average response time per day ( weekly average ' + barAvg.toFixed(3) + 's )');
     
     });
-    
   }
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  /*
-  //get context
-  //var context = $("#lineChart").get(0).getContext("2d");
-  //var context2 = $("#barChart").get(0).getContext("2d");
-  //doesn't work, fix it later:
-  //var options = { legendTemplate : "<ul id=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].pointColor%>\"><%if(datasets[i].label){%><%=datasets[i].label%><%}%></span></li><%}%></ul>" };
-
-  var ajaxData = {
-      'action': 'get_chart_data',
-  };
-
-  var chartLabel = [];
-  var lineValue = [];
-  var barValue = [];
-  var myLineChart; //shows amount of requests
-  var myBarChart; //shows response time
-  var context; $.getJSON(ajaxurl, ajaxData, function(json){
-
-    $.each(json, function (i,value){
-
-      chartLabel.push(value.time);
-      lineValue.push(value.request_count);
-      barValue.push(value.avg_resp);
-    });
-  var context2;
-  var barAvg;
-  var lineAvg;
-  
-  
-  //the first time the chart is drawn
- 
-
-   var LineChartData = {
-      labels: chartLabel,
-      datasets: [
-       /** {
-            label: "PHP",
-            fillColor: "rgba(220,220,220,0.2)",
-            strokeColor: "rgba(220,220,220,1)",
-            pointColor: "rgba(220,220,220,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(220,220,220,1)",
-            data: chartValue
-
-        },* /
-        {
-            label: "Total requests",
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: lineValue
-        }
-      ]};
-    
-    var BarChartData = {
-      labels: chartLabel,
-      datasets: [{
-            label: "Responsetimes (in seconds)",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: barValue
-      }]
-    };
-    
-    context = $("#lineChart").get(0).getContext("2d");
-    context2 = $("#barChart").get(0).getContext("2d");
-    
-    barAvg = countAvg(barValue);
-    lineAvg = Math.round(countAvg(lineValue));
-    myLineChart = new Chart(context).Line(LineChartData); 
-    myBarChart = new Chart(context2).Bar(BarChartData);
-    
-    //var legend = myLineChart.generateLegend();
-    //$( '#chart-legend' ).html(legend);
-    $("#lineChartAvg").text('Number of requests per day (average ' + lineAvg + ')');
-    $("#barChartAvg").text('Average response time per day (week average ' + barAvg.toFixed(3) + 'ms )');
-
-
-  });
-  
-  //alter the amount of data shown
-  $("#btnSubmit").click(function(){
-    var ajaxData = {
-        'action': 'get_chart_data','amount':3
-      };
-      var newChartLabel = [];
-      var newLineValue = [];
-      var newBarValue = [];
-    
-    $.getJSON(ajaxurl, ajaxData, function(json){
-      $.each(json, function (i,value){
-        newChartLabel.push(value.time);
-        newLineValue.push(value.request_count);
-        newBarValue.push(value.avg_resp);
-      });
-      i = 0;
-      while (i < newBarValue.length) {
-      myBarChart[0].datasets[0].bars[i].value = newBarValue[i];
-      myBarChart[0].datasets[0].bars[i].label = newChartLabel[i];
-      myBarChart[0].update();
-      i++;
-      }
-    });
-   
-  });
-*/  
   function countAvg(array){
     var sum = 0;
     for(i = 0; i < array.length ; i++){
@@ -275,7 +143,5 @@ $( document ).ready(function(){
     }
     return (sum/array.length);
   }
-
-
 });
 })(jQuery);
--- wp-dashboard-request-stats.php
+++ wp-dashboard-request-stats.php
@@ -44,175 +44,154 @@ class dashboard_request_stats{
     return $drs_instance;
   }
 
+  /**
+  * Initialize the plugin
+  */
+  public function __construct() {
+    add_action( 'admin_enqueue_scripts', array( $this, 'init' ) );
+    add_action( 'wp_dashboard_setup', array( $this,'add_dashboard_widget') );
+    add_action( 'wp_ajax_get_chart_data',array( $this,'get_chart_data_callback') );
+  }
 
 
+  public function init( $page_hook ) {
 
-
-/**
- * Initialize the plugin
- */
-public function __construct() {
-  add_action( 'admin_enqueue_scripts', array( $this, 'init' ) );
-  add_action( 'wp_dashboard_setup', array( $this,'add_dashboard_widget') );
-  add_action( 'wp_ajax_get_chart_data',array( $this,'get_chart_data_callback') );
-}
-
-
-public function init( $page_hook ) {
-
-   //make sure the scripts/stylesheets are loaded only where needed
-  if ( 'index.php' == $page_hook ){
-    //external scripts
-    wp_register_script( 'chartjs', plugins_url( '/script/Chart.js' , __FILE__), null, null, true );
-    wp_register_script( 'drawjs', plugins_url( '/script/draw.js' , __FILE__), null, null, true );
-    wp_enqueue_script( 'chartjs' );
-    wp_enqueue_script( 'drawjs' );
-    //styles
-    wp_register_style( 'wpdrs_css', plugins_url('/styles/wpdrs.css', __FILE__) );
-    wp_enqueue_style( 'wpdrs_css');
+    //make sure the scripts/stylesheets are loaded only where needed
+    if ( 'index.php' == $page_hook ){
+      //external scripts
+      wp_register_script( 'chartjs', plugins_url( '/script/Chart.js' , __FILE__), null, null, true );
+      wp_register_script( 'drawjs', plugins_url( '/script/draw.js' , __FILE__), null, null, true );
+      wp_enqueue_script( 'chartjs' );
+      wp_enqueue_script( 'drawjs' );
+      //styles
+      wp_register_style( 'wpdrs_css', plugins_url('/styles/wpdrs.css', __FILE__) );
+      wp_enqueue_style( 'wpdrs_css');
 
     //wp_localize_script( 'ajax-script', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' )) );
+    }
+
+    else{
+      return;
+    }
   }
 
-  else{
-    return;
-  }
-
-}
-
-
-/**
- * Add a widget to the dashboard.
- * This function is hooked into the 'wp_dashboard_setup' action below.
- */
-public function add_dashboard_widget() {
-  wp_add_dashboard_widget(
+  /**
+  * Add a widget to the dashboard.
+  * This function is hooked into the 'wp_dashboard_setup' action below.
+  */
+  public function add_dashboard_widget() {
+    wp_add_dashboard_widget(
                  'wp-dashboard-request-stats',     // Widget slug.
                  'Request Stats',                  // Title.
                  array($this,'create_widget_content') // Display function.
         );
-}
-
-
-/**
- * Create the function to output the contents of our Dashboard Widget.
- */
-public function create_widget_content() {
-  /*// Display canvas.
-  echo '<div id="wpdrsCharts"></div><canvas id="lineChart" style="width:100%; height:100%"></canvas>';
-  echo '<canvas id="barChart" style="width:100%; height:100%"></canvas></div>';
- */
-  require_once('widget.php');
-}
-
-
-/**
- * Parse a logfile and return an array of day_data objects
- */
-private function parse_log_file( $path , $regexp ){
-
-  //if the log file is gzipped
-  if(preg_match('#gz#',$path)){
-    $lines = gzfile( $path );
-  }
-  else{
-    $lines = file( $path );
-  }
-  if($lines==false){
-    return false;
   }
 
-  $unit = new time_data;
-  $time_array = array();
-  $matches;
-  $resp_exp= "#[0-9]+.[0-9]+$#";
-  $res_sum=0;
-  foreach ( $lines as $line ){
-  //find and extract timestamp
-    if (preg_match( $regexp, $line, $matches )){
-      //convert the timestamp for easier sorting
-      $matches[0] = str_replace('/',' ',$matches[0]);
-      //this is done only once to set the previous
-      if( is_null($unit->time) ){
-        $unit->time = $matches[0];
-      }
-      //if date has changed, write to new date object
-      if( $unit->time != $matches[0] ){
-        //divide the sum of response times with requestcount
-        $unit->avg_resp = floatval($res_sum) / $unit->request_count;
-        // push old into array
-        // push old into array
-        $time_array[] = $unit;
-        $unit = new time_data;
-        $unit->time = $matches[0];
-        $res_sum=0;
-      }
+  /**
+  * Create the function to output the contents of our Dashboard Widget.
+  */
+  public function create_widget_content() {
+    require_once('widget.php');
+  }
 
-      if(preg_match( $resp_exp, $line, $matches )){
-        $res_sum = $res_sum + floatval($matches[0]);
-      }
-      $unit->request_count++;
-
-    /*if ( preg_match( $resp_exp , $line )){
-      $day->get_count++ ;
-    } elseif (preg_match( "/MISS/" , $line )){
-      $day->post_count++;
-    }*/
+  /**
+  * Parse a logfile and return an array of day_data objects
+  */
+  private function parse_log_file( $path , $regexp ){
+    //if the log file is gzipped
+    if(preg_match('#gz#',$path)){
+      $lines = gzfile( $path );
     }
+    else{
+      $lines = file( $path );
+    }
+    if($lines==false){
+      return false;
+    }
+
+    $unit = new time_data;
+    $time_array = array();
+    $matches;
+    $resp_exp= "#[0-9]+.[0-9]+$#";
+    $res_sum=0;
+    foreach ( $lines as $line ){
+      //find and extract timestamp
+      if (preg_match( $regexp, $line, $matches )){
+        //convert the timestamp for easier sorting
+        $matches[0] = str_replace('/',' ',$matches[0]);
+        //this is done only once to set the previous
+        if( is_null($unit->time) ){
+          $unit->time = $matches[0];
+        }
+        //if date has changed, write to new date object
+        if( $unit->time != $matches[0] ){
+          //divide the sum of response times with requestcount
+          $unit->avg_resp = floatval($res_sum) / $unit->request_count;
+          // push old into array
+          // push old into array
+          $time_array[] = $unit;
+          $unit = new time_data;
+          $unit->time = $matches[0];
+          $res_sum=0;
+        }
+
+        if(preg_match( $resp_exp, $line, $matches )){
+          $res_sum = $res_sum + floatval($matches[0]);
+        }
+        $unit->request_count++;
+      }
+    }
+    //divide the sum of response times with requestcount
+    //push last into array
+    $unit->avg_resp = floatval($res_sum) / $unit->request_count;
+    $time_array[] = $unit;
+    return $time_array;
   }
-  //divide the sum of response times with requestcount
-  //push last into array
-  $unit->avg_resp = floatval($res_sum) / $unit->request_count;
-  $time_array[] = $unit;
-  
-  return $time_array;
-}
 
 
-/**
- * Fetch the chart data
- */
-public function get_chart_data_callback() {
+  /**
+  * Fetch the chart data
+  */
+  public function get_chart_data_callback() {
+    //define logpath in wp-config
+    if( !defined( 'WPDRS_LOGPATH' ) ){
+      $log_location = dirname( ini_get( 'error_log' ) );
+    }
+    else{
+      $log_location = WPDRS_LOGPATH;
+    }
+  
+    $log_file = '/total-access.log*';
+    $log_files = glob( $log_location . $log_file ); // all available logfiles, including gzipped ones
+    $amount = self::DEFAULT_AMOUNT;
+    
+    //if the requested amount of days differs from default
+    if( isset($_GET['amount']) ){
+      $amount = $_GET['amount'];
+    }
+  
+    $unit_data = $this->get_log_data( $log_files, $amount );
+  
+    echo ( json_encode( $unit_data ) );
+    wp_die();
+  }
 
-  //define logpath in wp-config
-  if( !defined( 'WPDRS_LOGPATH' ) ){
-    $log_location = dirname( ini_get( 'error_log' ) );
-  }
-  else{
-    $log_location = WPDRS_LOGPATH;
-  }
-  
-  $log_file = '/total-access.log*';
-  $log_files = glob( $log_location . $log_file ); // all available logfiles, including gzipped ones
-  $amount = self::DEFAULT_AMOUNT;
-  
-  //if the requested amount of days differs from default
-  if( isset($_GET['amount']) ){
-    $amount = $_GET['amount'];
-  }
-  
-  $unit_data = $this->get_log_data( $log_files, $amount );
-  
-  echo ( json_encode( $unit_data ) );
-  wp_die();
-}
+  /**
+  * Remove duplicates and make sure the entries in the array are in proper order
+  */
 
-/**
- * Remove duplicates and make sure the entries in the array are in proper order
- */
-
-private function clean_array( $array ){
+  private function clean_array( $array ){
   
-  $temp_array = array();
-  $temp_array2 = array();
+    $temp_array = array();
+    $temp_array2 = array();
   
-  //get a list of unique dates
-  for( $x = 0; $x < count($array) ; $x++ ){
-     $temp_array[]= $array[$x]->time;
-  }
+    //get a list of unique dates
+    for( $x = 0; $x < count($array) ; $x++ ){
+      $temp_array[]= $array[$x]->time;
+    }
   
-  $temp_array = array_unique($temp_array);
-  $temp_array = array_values($temp_array);
+    $temp_array = array_unique($temp_array);
+    $temp_array = array_values($temp_array);
   //sort the array entries by date
   /*for( $x = 0; $x < count($temp_array); $x++ ){
     strtotime($temp_array[$x]);
@@ -220,61 +199,61 @@ private function clean_array( $array ){
   }*/
   //sort($temp_array);
   
-  //sort the array entries by date
-  usort($temp_array, function($item1, $item2) {
-    $ts1 = strtotime($item1);
-    $ts2 = strtotime($item2);
-    return $ts2 - $ts1;
-  });
-  //usort reverses the values,
-  $temp_array  = array_reverse($temp_array,false);
+    //sort the array entries by date
+    usort($temp_array, function($item1, $item2) {
+      $ts1 = strtotime($item1);
+      $ts2 = strtotime($item2);
+      return $ts2 - $ts1;
+    });
+    //usort reverses the values,
+    $temp_array  = array_reverse($temp_array,false);
 
-  //remove duplicates from array
-  foreach( $temp_array as $date ){
-    $asd = new time_data();
-    $asd->time = $date;
-    for( $x = 0; $x < count( $array );$x++ ){
-      if( $date == $array[$x]->time ){
+    //remove duplicates from array
+    foreach( $temp_array as $date ){
+      $asd = new time_data();
+      $asd->time = $date;
+      for( $x = 0; $x < count( $array );$x++ ){
+        if( $date == $array[$x]->time ){
           $asd->request_count = $asd->request_count + $array[$x]->request_count;
           $asd->avg_resp = $asd->avg_resp + $array[$x]->avg_resp;
+        }
       }
+      $temp_array2[] = $asd;
     }
-    $temp_array2[] = $asd;
+    $array = $temp_array2;
+    return $array;
   }
-  $array = $temp_array2;
-  return $array;
-}
-/**
- * Return desired amount of data specified in days
- * Eats an array
- */
+  /**
+  * Return desired amount of data specified in days
+  * Eats an array
+  */
 
-private function get_log_data( $logfiles, $amount ){
-  $time_exp = '#[0-3][0-9]/.{3}/20[0-9]{2}#';
-  $temp_array = array();
-  $unit_data = array();
-  if( count($logfiles) == 0 ){
-      return;
-  }
+  private function get_log_data( $logfiles, $amount ){
+    $time_exp = '#[0-3][0-9]/.{3}/20[0-9]{2}#';
+    $temp_array = array();
+    $unit_data = array();
+    if( count($logfiles) == 0 ){
+      return false;
+    }
     
-  foreach($logfiles as $file){
-    //returns the contents of a logfile
-    $temp_array = $this->parse_log_file( $file, $time_exp );
-    foreach($temp_array as $entry){
-      $unit_data[] = $entry;
-      //makes sure the correct amount of days is returned
-      //and that there are no duplicate entries in the array
-      if(count($unit_data) >= $amount){
-        $unit_data = $this->clean_array($unit_data); 
+    foreach($logfiles as $file){
+      //returns the contents of a logfile
+      $temp_array = $this->parse_log_file( $file, $time_exp );
+      foreach($temp_array as $entry){
+        $unit_data[] = $entry;
+        //makes sure the correct amount of days is returned
+        //and that there are no duplicate entries in the array
         if(count($unit_data) >= $amount){
-          break 2;
+          $unit_data = $this->clean_array($unit_data); 
+          if(count($unit_data) >= $amount){
+            break 2;
+          }
         }
       }
     }
-  }
 
-return $unit_data;
-}
+    return $unit_data;
+  }
 }
 
 $dashboard_request_stats = dashboard_request_stats::get_instance();
